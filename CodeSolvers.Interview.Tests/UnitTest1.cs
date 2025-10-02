ecd78211037bb58779061a1256d9b1345f6ff069
alceq <angellalcequiez@gmail.com>
2025-10-02 12:29:08 -0400

    Initial commit

---
+++ CodeSolvers.Interview.Tests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿//using SeleniumExtras.WaitHelpers;
+//using Selenium.Support;
+//using Selenium.Webdriver;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace CodeSolvers.Interview.Tests;
+
+public class Tests
+{
+
+    private IWebdriver driver;
+
+    [SetUp]
+    public void StartBrowser()
+    {
+        driver = new ChromeDriver();
+        
+     
+       
+        
+    }
+
+
+    [Test]
+    public void SauceDemoTest()
+    {
+        driver.Navigate().GoToUrl("https://www.saucedemo.com");
+        Assert.Pass();
+    }
+
+
+    [TearDown]
+    public void CloseBrowser(){
+        driver.Quit();
+    }
+}
+
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI; 
+
+namespace Test
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver?.Quit();
+            driver?.Dispose();
+            driver = null;
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver!.Navigate().GoToUrl("https://www.saucedemo.com");
+            Assert.That(driver.Title, Is.EqualTo("Swag Labs"));
+
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+
+            new WebDriverWait(driver, TimeSpan.FromSeconds(5))
+                .Until(d => d.Url.Contains("inventory"));
+
+            Assert.That(driver.Url, Does.Contain("inventory"));
+        }
+    }
+}
+
+
